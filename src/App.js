dd6f3624763bee402ff7e1cb8201415e9cf827dc
Andres <and.694@gmail.com>
2022-10-31 00:00:13 -0500

    App inits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [index, setIndex] = useState({
+		actual: null,
+		next: Math.floor(Math.random() * 11),
+	});
+
+	useEffect(() => {
+		const timer = setInterval(
+			() =>
+				setIndex((s) => ({
+					next: Math.floor(Math.random() * 11),
+					actual: s.next,
+				})),
+			[2000]
+		);
+
+		return () => clearInterval(timer);
+	}, []);
+
+	return (
+		<div className="app">
+			<div className="title">
+				<h1>Random Sheet</h1>
+			</div>
+			<div className="actual-container">
+				{index.actual !== undefined && <span>{`${notes[index.actual]}`}</span>}
+			</div>
+			<div className="next-container">
+				<span>{`Next: ${notes[index.next]}`}</span>
+			</div>
+		</div>
+	);
 }
 
 export default App;
